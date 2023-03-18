a7a4a0dfa6236a7a2f5552a8e6ba2aca6126bc01
Сергей <cirill2020@yandex.ru>
2023-03-18 19:06:50 +0700

    Исправил баги

--- source/js/modules/change-descriptio-visibility.js
+++ source/js/modules/change-descriptio-visibility.js
@@ -2,7 +2,7 @@ export const changeDescriptionVisibility = () => {
 
   const showingTextButton = document.querySelector('.about-company__button');
 
-  const textSnippet = document.querySelector('.about-company__text span');
+  const textSnippet = document.querySelector('[data-text-snippet]');
 
   if (window.matchMedia('(max-width: 767px)').matches) {
     textSnippet.dataset.textVisibilityState = 'js-hidden';
